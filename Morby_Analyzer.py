037afcd3ebf2a60d5b12ae3d7b777fc800e82319
Bryan Buksoontorn <bxbuksoontorn@gmail.com>
2025-07-09 07:34:53 +0000

    git push

--- Morby_Analyzer.py
+++ Morby_Analyzer.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+def inject_dm_sans():
+    st.markdown("""
+                <style>
+                @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');
+
+        html, body, [class*="css"]  {
+            font-family: 'DM Sans', sans-serif;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+inject_dm_sans()
+
 st.set_page_config(page_title="Morby Method Deal Analyzer", layout="centered")
 
 st.title("🏡 Morby Method Deal Analyzer")
